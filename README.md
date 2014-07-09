71d3165ca1f60b99007fa65504b6790374b65ed3
Greg Allen <me@jga.me>
2014-07-09 16:27:50 -0700

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ location-search
 ===============
 
 Javascript lib to add location autocomplete to a input box
+
+## Requirements
+
+* Google Maps API `<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places&.js"></script>`
+* jQuery
---
+++ dist/location-search.bower.js
@@ -0,0 +1,48 @@
+/*!
+ * location-search - Lib to add location autocomplete to a input box
+ * v0.0.1
+ * https://github.com/firstandthird/location-search
+ * copyright First+Third 2014
+ * MIT License
+*/
+/*global google*/
+$.fn.locationSearch = function(options) {
+  options = options || {};
+
+  function search(location, types, callback) {
+
+    var service  = new google.maps.places.AutocompleteService(null);
+
+    types = types || ['(cities)'];
+    service.getPlacePredictions({ input: location, types: types },
+      function(predictions, status) {
+        if (status=='OK') {
+          var locations = [];
+          for (var i=0; i<5; ++i) {
+            if (predictions[i]) {
+              var p = predictions[i].description;
+              locations.push({place: p});
+             }
+           }
+           callback(locations);
+         }
+       }
+     );
+   }
+
+   this.each(function() {
+     var el = $(this);
+
+     el.complete({
+       sourceKey: 'place',
+       query: function(query, callback) {
+         var self = this;
+         if (query !== '') {
+           search(query, options.types, function(suggestions){
+             callback.call(self, suggestions);
+           });
+         }
+       }
+    });
+  });
+};
